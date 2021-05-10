f26d18138014713a68907a2f6065282f9bfa38c7
TheFightaz <83822200+TheFightaz@users.noreply.github.com>
2021-05-10 12:36:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,15 @@
 
+package fightaz.cooldown;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fightaz.cooldown.event.onChat;
+
+public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new onChat(), this);
+	}
+
+}
